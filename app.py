2e47bbf9f340bddc1348c54784b29358d466042e
Mohammad Ali <134193466+mdalii69@users.noreply.github.com>
2024-12-26 21:22:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def main():
 
     # Home Menu
     if choice == "Home":
-        st.write("Welcome to the chatbot. Please type \"Hi\" or \"Help me\" to start the conversation.")
+        st.write("Welcome to Universe chatbot. Please type \"Hi\" or \"Help me\" to start the conversation.")
 
         # Check if the chat_log.csv file exists, and if not, create it with column names
         if not os.path.exists('chat_log.csv'):
